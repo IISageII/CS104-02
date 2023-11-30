5cd1b18de0c9bdf3baabee3159eaa4fe78b2ad30
IISageII <146123687+IISageII@users.noreply.github.com>
2023-11-30 13:08:00 -0500

    PythonQueue

---
+++ PythonQueue.py
@@ -0,0 +1,13 @@
+names=[]
+
+for nameCount in range(0,10):
+    name=input("Enter name: ")
+    names.append(name)
+    nameCount +=1
+
+print(names)
+
+for nameCount in range(0,10):
+    len(names)
+    print(names.pop(0))
+print(names)   
